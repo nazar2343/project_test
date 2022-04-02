8528ddda5ad00e2a1c3c9b6bdbfa19030164a940
nazar2343 <barannazar234@gmail.com>
2022-04-02 18:05:03 +0000

    commit

--- test-project/src/app/store.js
+++ test-project/src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "../features/userSlice"
+import { userSlice } from "../features/userSlice"
 
 
 export default configureStore({
     reducer: {
-        user: userReducer,
+        user: userSlice,
     },
 })
